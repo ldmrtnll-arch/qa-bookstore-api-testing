b932caee57fdecc104a7776110c55992d207d1fb
Lidia Martinelli <lidiamo@weg.net>
2026-07-31 12:46:24 -0300

    test: add negative user creation scenarios

--- test-data/users-data.ts
+++ test-data/users-data.ts
@@ -1,5 +1,23 @@
 export const validUserPassword = '***REMOVED***';
 
+export const invalidUserPassword = 'weak';
+
+export const userAlreadyExistsError = {
+  code: '1204',
+  message: 'User exists!',
+};
+
+export const passwordRequirementsError = {
+  code: '1300',
+  message:
+    "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.",
+};
+
+export const usernameAndPasswordRequiredError = {
+  code: '1200',
+  message: 'UserName and Password required.',
+};
+
 export function generateUniqueUsername(): string {
   return `qa_portfolio_${Date.now()}`;
 }
